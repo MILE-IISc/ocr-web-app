51909c7bf30ebacaf2fc6d0f45ca6161818ce287
Shiva Kumar H R <shivahr@gmail.com>
2021-01-25 15:15:10 +0530

    Formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { ScreenComponent } from './screen/screen.component';
 import { AuthGuard } from "./auth/auth.guard";
 
-
-
 const appRoutes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   { path: 'screen', component: ScreenComponent, canActivate: [AuthGuard] },
-   // { path: "auth", loadChildren: "./auth/auth.module#AuthModule"},
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
 ];
 
 @NgModule({
