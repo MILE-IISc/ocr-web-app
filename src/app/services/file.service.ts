94368584d6416c58c3628b8e1838446946fa4db5
sasi5875 <ayyavusasidharan@gmail.com>
2021-01-27 20:14:28 +0530

    Fix download issue!

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -6,21 +6,24 @@ import { AuthService } from '../auth/auth.service';
 export class FileService {
   XML_BACKEND_URL;
   DOWNLOAD_XML_BACKEND_URL;
-  constructor(private http: HttpClient, private authService: AuthService) {
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
     this.XML_BACKEND_URL = this.authService.BACKEND_URL + "/api/xml/";
     this.DOWNLOAD_XML_BACKEND_URL = this.authService.BACKEND_URL + "/api/downloadXml/";
   }
 
   downloadFile(fileName: any) {
     console.log("fileName in download XML", fileName);
-    return this.http.get<{ message: string; xmlData: any }>(this.XML_BACKEND_URL + fileName).toPromise();
+    return this.httpClient.get<{ message: string; xmlData: any }>(this.XML_BACKEND_URL + fileName).toPromise();
   }
 
   downloadZipFile() {
     const options: any = {
-      header: new HttpHeaders({'Content_Type': 'application/octet-stream'}),
-      responseType: 'blob'
+      header: new HttpHeaders({
+        'Content_Type': 'application/json',
+        'Accept': 'application/json'
+      }),
+      responseType: 'blob' as 'json'
     };
-    return this.http.get(this.DOWNLOAD_XML_BACKEND_URL, options).toPromise();
+    return this.httpClient.get<Blob>(this.DOWNLOAD_XML_BACKEND_URL, options).toPromise();
   }
 }
